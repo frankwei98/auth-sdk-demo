fdec5e58d5922026103bba6ff8da96aa74249c92
Frank Wei <3261732+frankwei98@users.noreply.github.com>
2021-07-13 09:56:25 +0000

    good to go

--- src/App.js
+++ src/App.js
@@ -1,55 +1,35 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { verify } from "@meta-network/auth-sdk";
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
+  const [token, setToken] = useState('');
+  const [aud, setAud] = useState('');
+  const [verifiedResult, setVRes] = useState('')
+  const [error, setError] = useState(null)
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+    if (!token || !aud) return;
+    console.info('token changed to: ', token);
+    try {
+      const res = verify(token, aud);
+      setVRes(res);
+      setError(null)
+    } catch (error) {
+      setError(error)
     }
-    getDate();
-  }, []);
+  }, [token, aud]);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <h1><code>@meta-network/auth-sdk</code> Demo</h1>
+      { error && <div>Error happened: { JSON.stringify(error) }</div> }
+      <input placeholder="Enter aud field for JWT..." onChange={e => setAud(e.target.value)}></input>
+      <textarea onChange={e => setToken(e.target.value)}></textarea>
+      { verifiedResult && <div>
+        <h2>Parsed successfully</h2>
+        <pre>{JSON.stringify(verifiedResult, null, 2)}</pre>
+
+      </div> }
     </main>
   );
 }
